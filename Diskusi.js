5d2f062b2a341dee89822c1a5740e93eab42185c
ajax-jquery <72850308+ajax-jquery@users.noreply.github.com>
2020-12-02 19:47:58 +0700

    Create Diskusi.js

---
+++ Diskusi.js
@@ -0,0 +1,14 @@
+(function() {
+                        var bloggerjs = document.createElement(&quot;script&quot;);
+                        bloggerjs.type = &quot;text/javascript&quot;;
+                        bloggerjs.async = true;
+                        bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_item.js&quot;;
+                        (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
+                    })();
+                (function() {
+                    var bloggerjs = document.createElement(&quot;script&quot;);
+                    bloggerjs.type = &quot;text/javascript&quot;;
+                    bloggerjs.async = true;
+                    bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_index.js&quot;;
+                    (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
+                })();
