877fd94112c623f79b8db67b839f90d9d886f27b
ajax-jquery <72850308+ajax-jquery@users.noreply.github.com>
2020-12-02 19:53:40 +0700

    Update and rename Diskusi.js to diskusi.js

---
+++ diskusi.js
@@ -0,0 +1,14 @@
+(function() {
+                        var bloggerjs = document.createElement("script");
+                        bloggerjs.type = "text/javascript";
+                        bloggerjs.async = true;
+                        bloggerjs.src = "//"+disqus_shortname+".disqus.com/blogger_item.js";
+                        (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(bloggerjs);
+                    })();
+                (function() {
+                    var bloggerjs = document.createElement("script");
+                    bloggerjs.type = "text/javascript";
+                    bloggerjs.async = true;
+                    bloggerjs.src = "//"+disqus_shortname+".disqus.com/blogger_index.js";
+                    (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(bloggerjs);
+                })();
